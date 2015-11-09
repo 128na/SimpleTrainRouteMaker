bc992741cac52ef62e22d4a515e2446e544d410c
128na <simutrans.128na@gmail.com>
2015-11-09 22:47:28 +0900

    upload

---
+++ designs.js
@@ -0,0 +1,38 @@
+var designs = [
+	{
+		title           : "default",
+		name            : "デフォルト（横）",
+		author          : "128Na",
+		description     : "普通の路線図です。",
+		type            : "left",
+		stationDistance : 30,
+		trainDistance   : 30
+	},
+	{
+		title           : "simple",
+		name            : "シンプル（横）",
+		author          : "128Na",
+		description     : "シンプルかつコンパクトな路線図です。",
+		type            : "left",
+		stationDistance : 20,
+		trainDistance   : 20
+	},
+	{
+		title           : "default-column",
+		name            : "デフォルト（縦）",
+		author          : "128Na",
+		description     : "普通の路線図です。",
+		type            : "top",
+		stationDistance : 30,
+		trainDistance   : 30
+	},
+	{
+		title           : "simple-column",
+		name            : "シンプル（縦）",
+		author          : "128Na",
+		description     : "シンプルかつコンパクトな路線図です。",
+		type            : "top",
+		stationDistance : 20,
+		trainDistance   : 20
+	}
+]
